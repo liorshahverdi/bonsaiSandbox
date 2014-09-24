62049ee16df32122e82edf3bff76ea470e7bad78
unknown <liorshahverdi@gmail.com>
2014-09-24 16:08:22 -0400

    trimming lines

--- 2048sandbox/d2game.js
+++ 2048sandbox/d2game.js
@@ -1,89 +1,71 @@
 var drawGrid = function(){
-		var back_grid = new Rect(0, 0, 375, 375);
+	var back_grid = new Rect(0, 0, 375, 375);
 	back_grid.addTo(stage);
 	back_grid.fill('gray');
-
 	var a=50; var b=50; var c=40; var d=100; 
-
 	var tile_0 = new Circle(a,b,c,d);
 	tile_0.addTo(stage);
 	tile_0.fill('white');
-
 	a+=90; 
 	var tile_1 = new Circle(a,b,c,d);
 	tile_1.addTo(stage);
 	tile_1.fill('white');
-
-
 	a+=90;
 	var tile_2 = new Circle(a,b,c,d);
 	tile_2.addTo(stage);
 	tile_2.fill('white');
-
 	a+=90;
 	var tile_3 = new Circle(a,b,c,d);
 	tile_3.addTo(stage);
 	tile_3.fill('white');
-
 	b+=90;
 	a=50;
 	var tile_4 = new Circle(a,b,c,d);
 	tile_4.addTo(stage);
 	tile_4.fill('white');
-
 	a+=90;
 	var tile_5 = new Circle(a,b,c,d);
 	tile_5.addTo(stage);
 	tile_5.fill('white');
-
 	a+=90;
 	var tile_6 = new Circle(a,b,c,d);
 	tile_6.addTo(stage);
 	tile_6.fill('white');
-
 	a+=90;
 	var tile_7 = new Circle(a,b,c,d);
 	tile_7.addTo(stage);
 	tile_7.fill('white');
-
 	b+=90;
 	a=50;
 	var tile_8 = new Circle(a,b,c,d);
 	tile_8.addTo(stage);
 	tile_8.fill('white');
-
 	a+=90;
 	var tile_9 = new Circle(a,b,c,d);
 	tile_9.addTo(stage);
 	tile_9.fill('white');
-
 	a+=90;
 	var tile_10 = new Circle(a,b,c,d);
 	tile_10.addTo(stage);
 	tile_10.fill('white');
 	a+=90;
-
 	var tile_11 = new Circle(a,b,c,d);
 	tile_11.addTo(stage);
 	tile_11.fill('white');
-
 	b+=90;
 	a=50;
 	var tile_12 = new Circle(a,b,c,d);
 	tile_12.addTo(stage);
 	tile_12.fill('white');
-
 	a+=90;
 	var tile_13 = new Circle(a,b,c,d);
 	tile_13.addTo(stage);
 	tile_13.fill('white');
-
 	a+=90;
 	var tile_14 = new Circle(a,b,c,d);
 	tile_14.addTo(stage);
 	tile_14.fill('white');
 	a+=90;
-
 	var tile_15 = new Circle(a,b,c,d);
 	tile_15.addTo(stage);
 	tile_15.fill('white');
