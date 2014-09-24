6c2ae0396d4f0fbc9325fe486ea0ac96de4a6eae
Lior <liorshahverdi@gmail.com>
2014-09-24 02:17:15 -0400

    having a good time

--- clickmyrect.js
+++ clickmyrect.js
@@ -1,4 +1,29 @@
-/**
- * Paths w/ drag events.
- */
- 
+var back_grid = new Rect(0, 0, 400, 400);
+back_grid.addTo(stage);
+back_grid.fill('green');
+var x1=10;
+var y1=10;
+var x2=80;
+var y2=80;
+/*
+var cell_0 = new Rect(x1,y1,x2,y2);
+cell_0.addTo(stage);
+cell_0.fill('brown');
+x1+=100; 
+var cell_1 = new Rect(x1,y1,x2,y2);
+cell_1.addTo(stage);
+cell_1.fill('brown');
+x1+=100; 
+var cell_2 = new Rect(x1,y1,x2,y2);
+cell_2.addTo(stage);
+cell_2.fill('brown');
+x1+=100; 
+var cell_3 = new Rect(x1,y1,x2,y2);
+cell_3.addTo(stage);
+cell_3.fill('brown');
+x1=10;
+y1=110;
+var cell_4 = new Rect(x1,y1,x2,y2);
+cell_4.addTo(stage);
+cell_4.fill('brown');
+*/
