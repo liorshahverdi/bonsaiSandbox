90a42de87454c62fdd9729d46082c429f8eddd2c
unknown <liorshahverdi@gmail.com>
2014-09-24 14:57:21 -0400

    game grid drawn

--- movie1.js
+++
@@ -1,5 +0,0 @@
-var myAnimation = new Animation('1s', {
-  x: 190 // animate `x` attribute to 190
-});
-
-myDisplayObject.animate(myAnimation); // apply myAnimation to `myDisplayObject`
---
+++ theGame.js
@@ -0,0 +1,2 @@
+var g = "Fred"!;
+console.log( 'hi '+g);
