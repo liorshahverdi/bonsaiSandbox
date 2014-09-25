18f4f16470aed8208090623da72050576e9fba00
unknown <liorshahverdi@gmail.com>
2014-09-25 15:47:40 -0400

    32 64 and 128 drawn

--- 2048sandbox/d2game.js
+++ 2048sandbox/d2game.js
@@ -1,4 +1,4 @@
-var dg = function(){
+var draw_grid = function(){
 	/*var frame = new Rect(0,0,380, 380);
 	frame.addTo(stage);
 	frame.fill('blue');
@@ -201,9 +201,58 @@ var draw_a_32_node = function(posx, posy) {
 	});
 	stage.addChild(text);
 }
-//dg();
+
+var draw_a_64_node = function(posx, posy) {
+	// 50 50 is at grid[0][0]
+	var node_backbun = new Circle(posx, posy, 40, 100);
+	node_backbun.addTo(stage);
+	node_backbun.fill('indigo');
+
+	var node_meat = new Circle(posx, posy, 35, 100);
+	node_meat.addTo(stage);
+	node_meat.fill('white');
+
+	var node_frontbun = new Circle(posx, posy, 30, 100);
+	node_frontbun.addTo(stage);
+	node_frontbun.fill('indigo');
+
+	var text = new Text('64');
+	text.attr({
+	  x: (posx-14), y: (posy - 9), textFillColor: 'black', opacity: 0.5,
+	  fontFamily: 'Arial', fontSize: 25, fontWeight: 'bold'
+	});
+	stage.addChild(text);
+}
+
+var draw_a_128_node = function(posx, posy) {
+	// 50 50 is at grid[0][0]
+	var node_backbun = new Circle(posx, posy, 40, 100);
+	node_backbun.addTo(stage);
+	node_backbun.fill('violet');
+
+	var node_meat = new Circle(posx, posy, 35, 100);
+	node_meat.addTo(stage);
+	node_meat.fill('white');
+
+	var node_frontbun = new Circle(posx, posy, 30, 100);
+	node_frontbun.addTo(stage);
+	node_frontbun.fill('violet');
+
+	var text = new Text('128');
+	text.attr({
+	  x: (posx-20), y: (posy - 9), textFillColor: 'black', opacity: 0.5,
+	  fontFamily: 'Arial', fontSize: 25, fontWeight: 'bold'
+	});
+	stage.addChild(text);
+}
+
+
+
+draw_grid();
 draw_a_2_node(50,50);
 draw_a_4_node(50,140); //[50][140] is at grid [1][0]
 draw_a_8_node(50, 230);//[50][230] is at grid [2][0]
 draw_a_16_node(50, 320);//[50][320] is at grid [3][0]
 draw_a_32_node(140, 50);//[50][320] is at grid [3][0]
+draw_a_64_node(140, 140);
+draw_a_128_node(140, 230);
